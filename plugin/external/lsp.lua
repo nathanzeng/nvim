115e11fd10ffbb25ea4933d615f51e76b1aba3cf
Nathan Zeng <nathan.j.zeng@gmail.com>
2026-05-16 23:02:24 -0700

    lsp: put love2d in comments

--- plugin/external/lsp.lua
+++ plugin/external/lsp.lua
@@ -294,8 +294,7 @@ local servers = {
             vim.env.VIMRUNTIME,
             -- Depending on the usage, you might want to add additional paths
             -- here.
-            -- '${3rd}/luv/library',
-            -- '${3rd}/busted/library',
+            -- '${3rd}/love2d/library',
           },
           -- Or pull in all of 'runtimepath'.
           -- NOTE: this is a lot slower and will cause issues when working on
