80475dbae2f190ab2bd7566ba1127ea788d40843
Nathan Zeng <nathan.j.zeng@gmail.com>
2026-07-08 11:34:14 -0700

    leap: use ,

--- plugin/external/leap.lua
+++ plugin/external/leap.lua
@@ -1,3 +1,4 @@
 -- NOTE: not setting up leap from window or remote
 vim.pack.add({ 'https://codeberg.org/andyg/leap.nvim.git' })
-vim.keymap.set({ 'n', 'x', 'o' }, '\\', '<Plug>(leap)')
+vim.keymap.set('n', [[\]], ',', { desc = 'Repeat latest f, t, F or T in opposite direction' })
+vim.keymap.set({ 'n', 'x', 'o' }, ',', '<Plug>(leap)')
